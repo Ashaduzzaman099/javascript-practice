634687592fd1f5b8378dec5d5049c959b8423863
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-17 22:46:04 +0600

    Practice Rest  Operator

--- ES6 practice/restOperator.js
+++ ES6 practice/restOperator.js
@@ -6,3 +6,18 @@ console.log(rest);
 const [first2, secend, ...rest2] = [1, 2, 3, 4, 5];
 console.log(secend);
 console.log(rest2);
+
+// Function with rest parameter
+const sum = (...numbers) => {
+    console.log(numbers);
+}
+
+sum(1, 2, 3, 4, 5);
+
+// Arrow Function with rest and reduce to calculate total of array items
+const total = (...nums) => {
+    return nums.reduce((num,sum) =>{
+        return num+sum;
+    },0)
+}
+console.log(total(3,1,5,3))
