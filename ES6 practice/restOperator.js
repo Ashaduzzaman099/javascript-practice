0b2e38a5770659f6840157f96d3300fa65edeb0a
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-17 22:31:50 +0600

    Practice Rest  Operator

---
+++ ES6 practice/restOperator.js
@@ -0,0 +1,8 @@
+const [first, ...rest] = [1, 2, 3, 4, 5];
+console.log(first);
+console.log(rest);
+
+
+const [first2, secend, ...rest2] = [1, 2, 3, 4, 5];
+console.log(secend);
+console.log(rest2);
