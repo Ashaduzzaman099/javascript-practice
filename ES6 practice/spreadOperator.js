038cb59173b9dff0387fecd1016c7e0ddb7cbfc8
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-17 11:51:47 +0600

    Arry Combine

--- ES6 practice/spreadOperator.js
+++ ES6 practice/spreadOperator.js
@@ -5,3 +5,10 @@ console.log(newNums);
 // Add New Items
 const addNewNums = [8, ...nums, 7];
 console.log(addNewNums);
+
+// Arry Combine
+const arryA = [1, 2, 3, 5, 6];
+const arryB = [7, 8, 9, 10];
+
+const combineArry = [...arryA, ...arryB];
+console.log(combineArry);
