afed01a0de961792ae8d4570909263dbd36489a1
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-17 21:55:43 +0600

    Update Async

--- functions/asyncFatchData.js
+++ functions/asyncFatchData.js
@@ -7,11 +7,18 @@ async function getData() {
 
     const data = await response.json()
 
+    // Using Map function to print the name of the users
     data.map(user => {
       console.log(user.name);
     })
 
+    // Using ForEach function to print the name of the users
+    data.forEach(user => {
+      console.log(user.name);
+    }) ;
+
   } catch(error) {
+    
 
     console.log("Error happened")
 
